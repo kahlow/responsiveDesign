07bbf23c76a60fbfb42138590adfe71037518e23
kahlow <tkahlow@gmail.com>
2013-06-09 12:16:19 -0400

    bacon ipsum was giving me problems with the layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
 $(document).ready(function() {
-  $('.leftColumn').BaconIpsum({ type:'meat', paras:3, start_with_lorem:true });
-  $('.rightColumn').BaconIpsum({ type:'meat', paras:3, start_with_lorem:true });
+
 });
