8deade7e8a1d10db59ec0b0082718d01b6921330
kahlow <tkahlow@gmail.com>
2013-06-09 10:46:22 -0400

    starting the responsive design process

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $('.leftColumn').BaconIpsum({ type:'meat', paras:3, start_with_lorem:true });
+  $('.rightColumn').BaconIpsum({ type:'meat', paras:3, start_with_lorem:true });
+});
